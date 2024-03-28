4afb4b6f69a9dd447cf120a1f32d7e5978762839
unknown <desarrollador1aj@gmail.com>
2024-03-28 15:55:15 -0500

    Escala

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,19 +6,19 @@ import { useRef } from "react";
 export const Experience = () => {
   const boxRef = useRef(null);
 
-  useFrame(({clock}, delta)=>{
-    //boxRef.current.rotation.x += 1 * delta;
-    boxRef.current.rotation.x += (Math.PI/2) * delta;
-    boxRef.current.rotation.y += Math.PI * delta;
-    boxRef.current.position.y = Math.cos(clock.getElapsedTime());
-    boxRef.current.position.x += 0.5 * delta;
+  useFrame(({clock},delta)=>{
+        boxRef.current.rotation.y += delta *0.1;     
   })
-
+  
   return (
     <>
         <ambientLight intensity={2} />
         <directionalLight position={[10,10,5]} intensity={2}/>
-        <mesh ref={boxRef}>
+        <mesh ref={boxRef} 
+              position={[0,0,0]} 
+              rotation={[0, Math.PI/2 , 0]}
+              scale={[10,1,10]}>
+
             <boxGeometry args={[1,1,1]}/> 
             <meshStandardMaterial color={"purple"}/>
         </mesh>
