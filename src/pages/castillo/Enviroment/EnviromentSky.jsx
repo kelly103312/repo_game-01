8a40b85a1bb4b03e1b25bd30bd342a5154e752ee
unknown <desarrollador1aj@gmail.com>
2024-04-23 22:17:30 -0500

    Level a presentar

--- src/pages/castillo/Enviroment/EnviromentSky.jsx
+++ src/pages/castillo/Enviroment/EnviromentSky.jsx
@@ -1,9 +1,15 @@
-import { Sparkles } from '@react-three/drei'
+import { Sky, Sparkles } from '@react-three/drei'
 import React from 'react'
 
 export const EnviromentSky = () => {
   return (
     <>
+        <Sky 
+            distance={450000}
+            sunPosition={[1,-1, 1]} 
+            inclination={0.2} 
+            elevation={5}
+            azimuth={360}/>
         <Sparkles 
             position={[0,1.5,-96]}
             color= "yellow"
--- src/pages/castillo/Lights/Ligths.jsx
+++ src/pages/castillo/Lights/Ligths.jsx
@@ -5,25 +5,20 @@ export const Ligths = () => {
   return (
     <>
         <ambientLight 
-          intensity={0.5} 
+          intensity={1} 
         />
         <directionalLight 
             castShadow={true}
-            color={new Color("#47CEEF")}
-            position={[2,10,0]} 
-            intensity={6}
+            color={new Color("#71B2F3")}
+            position={[2,8,0]} 
+            intensity={8}
             shadow-mapSize={[2048,2048]}
-            shadow-camera-far={10}
-            shadow-camera-left={-16}
-            shadow-camera-right={16}
-            shadow-camera-top={16}
-            shadow-camera-bottom={-16}
         /> 
         <hemisphereLight 
             position={[2,8,-2]}
-            intensity={0.8}
-            skyColor={new Color("#F01414")}
-            groundColor={new Color("#FFFFFF")}
+            intensity={2}
+            skyColor={new Color("#3E68DB")}
+            groundColor={new Color("#806A12")}
          />
     </>
   )
